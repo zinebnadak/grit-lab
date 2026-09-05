64c6e1036eca3f99db84bb46ae707d80118ace1e
zinebnadak <zineb@nadak.ai>
2026-09-05 14:14:04 +0300

    task: skapa en funtion som tar nuvanande head Node och önskad head våde, lägger till en nod i början av listan, eg uppdatera head till en ny nod och låt nya head peka på den gamla noden

--- linked_lists/linked_lists.py
+++ linked_lists/linked_lists.py
@@ -3,21 +3,29 @@ class Node:
         self.data = data 
         self.next = None 
 
+# skapa länkad lista
 node_1 = Node(5)
-
-head = node_1
 node_2 = Node(10)
 node_3 = Node(15)
 
 node_1.next = node_2
 node_2.next = node_3
 
-# Hela poängen med en länkad lista är att du inte har en extern lista med alla noder 
-# ,du har bara head, och måste ta dig vidare via .next för att hitta nästa nod.
+# Insert-at-beginning: lägga till en ny nod före nuvarande head.
+# Tanken: den nya noden ska peka på det som var head, och sen ska head uppdateras till att peka på den nya noden.
+# head / node = ger alltså det HELA elementet som den pekar på ex node_1
+# node.data ger alltså elementets faktiska värde ex 5
 
-# Kolla None-villkoret på node-variabeln själv, inte på node.next, och gör kollen innan du printar
+def insert_at_beginning(head, data):    # this function takes current ehead and inserts the node (data) you want to be first
+    new_node = Node(data)
+    new_node.next = head
+    return new_node
+    
+# testa 
+head = node_1
+new_head = insert_at_beginning(head, 0)
 
-current_node = head # start at head 
+current_node = new_head # start at head 
 
 while current_node != None: # continue until node.next is None
     print(current_node.data) # print its actuall content
