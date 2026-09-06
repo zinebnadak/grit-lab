ee3cea4e979a1edfd16fd7a4c16aaa8658594e52
zinebnadak <zineb@nadak.ai>
2026-09-06 15:57:57 +0300

    REPETITION

--- linked_lists/linked_lists.py
+++ linked_lists/linked_lists.py
@@ -1,32 +1,43 @@
+# 1. Skapa en klass och länka noderna
 class Node:
-    def __init__(self,data):
-        self.data = data 
+    def __init__(self, data):
+        self.data = data
         self.next = None 
 
-# skapa länkad lista
-node_1 = Node(5)
-node_2 = Node(10)
-node_3 = Node(15)
+    
+node_1 = Node(1)
+node_2 = Node(2)
+node_3 = Node(3)
 
+head = node_1
+
+# skapa linked lista 
 node_1.next = node_2
 node_2.next = node_3
 
-# Insert-at-beginning: lägga till en ny nod före nuvarande head.
-# Tanken: den nya noden ska peka på det som var head, och sen ska head uppdateras till att peka på den nya noden.
-# head / node = ger alltså det HELA elementet som den pekar på ex node_1
-# node.data ger alltså elementets faktiska värde ex 5
+'''
+# skriv ut
+print(head.data, node_2.data, node_3.data)
+'''
 
-def insert_at_beginning(head, data):    # this function takes current ehead and inserts the node (data) you want to be first
-    new_node = Node(data)
-    new_node.next = head
-    return new_node
-    
-# testa.
-head = node_1
+'''
+# 2. loopa igenom från head till slut, UTAN att använda en lista
+current_node = head
+while current_node != None:
+    print(current_node.data)
+    current_node = current_node.next
+'''
+
+# 3. funktion som byter ut/ sätter in element i början 
+
+def insert_at_beginning(current_head, value):
+    new_head = Node(value)
+    new_head.next = current_head
+    return new_head 
+
 new_head = insert_at_beginning(head, 0)
+current_node = new_head
 
-current_node = new_head # start at head 
-
-while current_node != None: # continue until node.next is None
-    print(current_node.data) # print its actuall content
-    current_node = current_node.next # update the value of "node"
+while current_node != None:
+    print(current_node.data)
+    current_node = current_node.next
