58b69540756a605652b032659ec9e90769f1c0fa
zinebnadak <zineb@nadak.ai>
2026-09-06 17:05:28 +0300

    task: skapa en typ i Go som heter Node med två attribut. data och next och sedan skapa tre instanser av dem och länka dem med varandra. Kom ihåg att node instanserna är av typen *Node och du ger dem endast en adress med &. skriv ut nod PEKAREN endast

---
+++ linked_lists/linked_lists.go
@@ -0,0 +1,41 @@
+// kör med go run linked_lists/linked_lists.go
+package main
+import "fmt"
+
+// nil i Go, None i Python
+// define new type with "type"
+// Node is the name of the type 
+// struct to define the structure of the type. Inside the attribute names you want the type to have and the Go types they are 
+// no __init__ instructor needed
+// this is just a declaration and does not need to be inside func main()
+
+type Node struct {
+	data int 
+	next *Node // explicit pekare till en annan Node av typ pekare, precis som typen som görs när vi skapar en Node
+}
+
+
+func main () {
+
+	// creating an instans of a Node this way
+
+	node_3 := &Node{data: 15,next: nil}
+	node_2 := &Node{data: 10,next: node_3}
+	node_1 := &Node{data: 5,next: node_2} 
+
+	// &Node{...} skapar en Node och & tar adressen till den, så n1 blir av typen *Node (en pekare), inte Node direkt.
+	// Analogi: tänk dig ett hus (Node-structen med dess data) och dess gatuadress (pekaren *Node). 
+	// Node{...} bygger huset. &Node{...} ger dig lappen med adressen till huset , inte huset självt.
+
+	// &-operatorn: tar ett hus (en struct) och ger dig adressen till det (struct -> pekare)
+	// *-operatorn (dereferensering): gör tvärtom ,tar en adress (pekare) och ger dig huset som ligger där (pekare -> struct)
+
+	fmt.Println(node_1)
+	fmt.Println(*node_1)
+	
+	fmt.Println(node_2) // en adress skapad med &
+	fmt.Println(*node_2) // en pekare skriver ut structen
+
+	fmt.Println(node_3)
+	fmt.Println(*node_3)
+}
