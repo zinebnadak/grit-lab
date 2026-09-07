7ccb31ea214b8166c33ea7d6915e2494f836d53b
zinebnadak <zineb@nadak.ai>
2026-09-07 14:08:57 +0300

    REPETITION

--- linked_lists/linked_lists.go
+++ linked_lists/linked_lists.go
@@ -1,40 +1,49 @@
-// kör med go run linked_lists/linked_lists.go
 package main
 import "fmt"
 
+// creating a type with two attributes: data and next
+
 type Node struct {
-	data int 
-	next *Node 
+	data int
+	next *Node
 }
 
-// function for inserting 
-func add_to_end (head *Node, value int) *Node {
-	new_node := &Node{data: value, next: nil}
+// function for insertting at the end
 
-	current := head 
-	for current.next != nil { // OBS: här VILL vi kolla .next, till skillnad från print-loopen!
-	current = current.next
+func add_to_end (head *Node, value int) *Node {
+	new_node := &Node{data: value, next: nil} // we set next to nil bcs it will be the last oen
+
+	current := head
+	for current.next != nil { // gå igenom hela listan tills vi med nil identify when we are at the end 
+		current = current.next
 	}
 
-	current.next = new_node
-	return head // returnera hela listan igen med start fån head
+	current.next = new_node // efter for loopen klar blir sista nodesa next det nya värdet
+	return head // return the new head so that we can assign the old head with the new one only by calling the function
 }
 
-
+// Creating the linked list: creating three instanses of the type: Node. Remember the referenced in next needs to be defined before its used, bcs Go reads from top to bottom. 
 func main () {
 
-	node_3 := &Node{data: 15,next: nil}
-	node_2 := &Node{data: 10,next: node_3}
-	node_1 := &Node{data: 5,next: node_2} 
+	node_3 := &Node{data: 3, next: nil}
+	node_2 := &Node{data: 2, next: node_3}
+	node_1 := &Node{data: 1, next: node_2}
+
+	/*
+	fmt.Println(node_3) //skriver ut pekaren med adressen 
+	fmt.Println(node_2.data) //skriver ut datan
+	fmt.Println(node_1.next) // skriver ut nästa nodens pekare
+	*/
 
 	head := node_1
+	head = add_to_end(head,4) //updating the head/linked list (with a new added element)
+
 	current_node := head
 
-	head = add_to_end(head, 20) // enkelt anrop, ingen loop behövs här
-
-	// traversera och skriv ut hela listan för att verifiera
 	for current_node != nil {
 		fmt.Println(current_node.data)
 		current_node = current_node.next
 	}
 }
+
+
