d01898649ad1e1d2528f6e619a2cff9790777716
znadak <zineb.nadak@gritlab.ax>
2026-09-09 18:59:46 +0300

    task: skapa en custom datastructur av en nod i ett binärt träd

--- binary_search_trees/BST.go
+++ binary_search_trees/BST.go
@@ -1,5 +1,16 @@
-/* en rekursiv funktion är en funktion som anropar sig självt
-Innehåller alltid två delar:
-- Bas-fall 
-- Rekursivt fall 
+/*
+Rekursion används i BST naturnligt eftersom ett träd är uppbyggt av mindre träd (subträd) 
+som i sin tur är uppbyggda av ännu mindre träd.
+*/
+
+type Node struct {
+	data int
+	left *Node
+	right *Node
+}
+
+/*
+BST-regeln (Binary Search Tree property): för varje nod gäller att allt i dess vänstra subträd är mindre än noden
+, och allt i dess högra subträd är större eller lika.
+Exempel (från videon): rot = 4, vänster subträd innehåller 1,2,3 (alla mindre än 4), höger subträd innehåller 5,6,7 (alla större)
 */
