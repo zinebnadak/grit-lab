8992192bc1f5165c6647da0ed18c38a614818d8d
zinebnadak <zineb@nadak.ai>
2026-09-21 12:35:38 +0300

    Task: Lastword. Starting from end

--- CHECKPOINT_EXERCISES/lastword/piscine/lastword.go
+++ CHECKPOINT_EXERCISES/lastword/piscine/lastword.go
@@ -1,13 +1,15 @@
 package piscine
+
 func LastWord(s string) string {
-	word := ""
-	for i := 0; i < len(s); i++ {
-		if s[i] != ' ' { // has to be a letter
-			if i > 0 && s[i-1] == ' ' {  // starting of a new word eg. previous char was a space. Beginning of the string (0) does not have a previous char worth checking
-				word = "" // clear when starting of a new word
+	end := 0
+	for i := len(s) - 1; i >= 0; i-- {
+		if s[i] != ' ' { // s[i] is the character, i is its index
+			if end == 0 { // still no word found yet
+				end = i + 1 // ending index
 			}
-			word += string(s[i]) // add letter
+		} else if end != 0 { // a space after the word: its start is i+1
+			return s[i+1:end] + "\n"
 		}
 	}
-	return word + "\n"
+	return s[:end] + "\n" // no space found after the word, so it starts at 0
 }
