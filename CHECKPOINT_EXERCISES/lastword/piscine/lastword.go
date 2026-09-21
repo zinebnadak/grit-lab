a222e8ac3eb7ba363da6632105eccbe18287f857
zinebnadak <zineb@nadak.ai>
2026-09-21 11:49:27 +0300

    Task: Lastword, own original solution

--- CHECKPOINT_EXERCISES/lastword/piscine/lastword.go
+++ CHECKPOINT_EXERCISES/lastword/piscine/lastword.go
@@ -1,14 +1,13 @@
 package piscine
-
 func LastWord(s string) string {
-	start, end := 0, 0
-	for i, char := range s {
-		if char != ' ' {
-			end = i + 1
-			if i > 0 && s[i-1] == ' ' {
-				start = i
+	word := ""
+	for i := 0; i < len(s); i++ {
+		if s[i] != ' ' { // has to be a letter
+			if i > 0 && s[i-1] == ' ' {  // starting of a new word eg. previous char was a space. Beginning of the string (0) does not have a previous char worth checking
+				word = "" // clear when starting of a new word
 			}
+			word += string(s[i]) // add letter
 		}
 	}
-	return s[start:end] + "\n"
+	return word + "\n"
 }
