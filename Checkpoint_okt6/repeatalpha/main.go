d2e97860951e7ee975cd8409307740a36bdf5db1
zinebnadak <zineb@nadak.ai>
2026-09-23 19:35:09 +0300

    wip: repeatalpha

---
+++ Checkpoint_okt6/repeatalpha/main.go
@@ -0,0 +1,20 @@
+
+
+
+package main
+
+import (
+	"fmt"
+	"piscine"
+)
+
+func main() {
+	fmt.Println(piscine.RepeatAlpha("abc"))
+	fmt.Println(piscine.RepeatAlpha("Choumi."))
+	fmt.Println(piscine.RepeatAlpha(""))
+	fmt.Println(piscine.RepeatAlpha("abacadaba 01!"))
+}
+
+
+func RepeatAlpha(s string) string {
+}
