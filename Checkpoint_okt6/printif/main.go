0ab1158f9412b54447485ff279ab8aa1376cfa9f
zinebnadak <zineb@nadak.ai>
2026-09-25 11:21:28 +0300

    wip: printif

---
+++ Checkpoint_okt6/printif/main.go
@@ -0,0 +1,20 @@
+// Input: 
+// Output:
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Print(PrintIf("abcdefz"))
+	fmt.Print(PrintIf("abc"))
+	fmt.Print(PrintIf(""))
+	fmt.Print(PrintIf("14"))
+}
+
+
+func PrintIf(str string) string {
+
+}
